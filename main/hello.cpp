4ffc9bd59e624aa9211ce4a1fa57151c95a89bd9
Athul <113627215+athulraimon@users.noreply.github.com>
2024-03-08 16:43:13 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "PES1UG21CS125\n";
+  cout << "Change\n";
   return 0;
 }
